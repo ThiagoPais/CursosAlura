0bbe2417db5cc0f42116da305ef297ba2152508e
Thiago Cardoso <thiagocpais@hotmail.com>
2024-12-05 15:01:28 -0300

    Implementação básica da classe Produto

--- curso-maven/maven-gerenciamento-dependencias/src/main/java/br/com/alura/loja/Produto.java
+++ curso-maven/maven-gerenciamento-dependencias/src/main/java/br/com/alura/loja/Produto.java
@@ -1,5 +1,31 @@
 package br.com.alura.loja;
 
+import java.math.BigDecimal;
+
 public class Produto {
+
+    private String name;
+    private BigDecimal price;
+
+    public Produto(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 
 }
