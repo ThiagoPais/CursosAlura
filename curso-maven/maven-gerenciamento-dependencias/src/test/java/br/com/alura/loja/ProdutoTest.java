cbe624baade7247760a2092c0af820decf898241
Thiago Cardoso <thiagocpais@hotmail.com>
2024-12-05 15:04:22 -0300

    Implementação de teste simples

--- curso-maven/maven-gerenciamento-dependencias/src/test/java/br/com/alura/loja/ProdutoTest.java
+++ curso-maven/maven-gerenciamento-dependencias/src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -1,15 +1,20 @@
-/* package br.com.alura.loja;
+package br.com.alura.loja;
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
 
 //import static org.junit.Assert.*;
 
-//import org.junit.Test;
+import org.junit.Test;
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Produto p = new Produto("teste", BigDecimal.TEN);
+		Assert.assertEquals("teste", p.getName());
+		Assert.assertEquals(BigDecimal.TEN, p.getPrice());
 	}
 
 }
- */
