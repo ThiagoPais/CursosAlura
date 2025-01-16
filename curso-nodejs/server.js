822acb36124d809fe00405360fb4fe3c9bdc2d76
Thiago Cardoso <thiagocpais@hotmail.com>
2025-01-16 16:13:05 -0300

    Configurando rotas iniciais

--- curso-nodejs/server.js
+++ curso-nodejs/server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const routes = {
+    "/": "Curso de Node.js",
+    "/livros": "Página de livros",
+    "/autores": "Página de autores"
+};
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Curso de Node.js")
+    res.end(routes[req.url]);
 });
 
 server.listen(PORT, () => {
