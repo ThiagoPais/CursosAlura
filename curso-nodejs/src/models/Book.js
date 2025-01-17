1bb6dbaef2ce8db888791ed2c11e7141a93f270a
Thiago Cardoso <thiagocpais@hotmail.com>
2025-01-17 17:16:04 -0300

    book schema

---
+++ curso-nodejs/src/models/Book.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { required } from "nodemon/lib/config";
+
+const bookSchema = new mongoose.Schema({
+    id: {type: mongoose.Schema.Types.ObjectId},
+    title: {type: String, required: true},
+    publicher: {type: String, required: true},
+    pages: {type: Number, required: true},
+    price: {type: Number}
+}, {versionKey: false});
+
+const book = mongoose.model("books", bookSchema);
+
+export default book;
